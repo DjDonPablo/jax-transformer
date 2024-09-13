7291adc353ca88b1d327a2cad947c6ca671d49dc
Maël Reynaud <mael.reynaud@epita.fr>
2024-09-13 20:27:32 +0200

    [main] update: config and utils

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
-class Config:
+class TransformerConfig:
     def __init__(
         self,
-        vocab_size: int = 30256,
+        vocab_size: int = 10256,
         vocab_path: str = "data/vocab.txt",
         weights_path: str = "TO_BE_DEFINED",
-        embedding_size: int = 512,
+        embedding_dim: int = 512,
+        key_query_dim: int = 64,
         context_size: int = 512,
         nb_blocks: int = 6,
+        nb_heads: int = 8,
         batch_size: int = 64,
         warmup_steps: int = 4000,
         beta1: float = 0.9,
@@ -14,15 +16,19 @@ class Config:
         epsilon: float = 1e-9,
         dropout_rate: float = 0.1,
         label_smoothing: float = 0.1,
+        top_k: int = 5,
+        temp: float = 1.0,
     ) -> None:
         self.vocab_path = vocab_path
         self.weights_path = weights_path
 
         # constants
         self.vocab_size = vocab_size
-        self.embedding_size = embedding_size
+        self.embedding_dim = embedding_dim
+        self.key_query_dim = key_query_dim
         self.context_size = context_size
         self.nb_blocks = nb_blocks
+        self.nb_heads = nb_heads
 
         # training
         self.batch_size = batch_size
@@ -35,6 +41,10 @@ class Config:
         self.beta2 = beta2
         self.epsilon = epsilon
 
+        # inference
+        self.top_k = top_k
+        self.temp = temp  # acts on softmax
+
         # ~ 1e5 steps for training
 
     def get_model_size(self) -> int:
@@ -46,5 +56,5 @@ class Config:
         )
 
 
-config = Config()
+config = TransformerConfig()
 print(config.get_lr(2000))
--- utils.py
+++ utils.py
@@ -1,7 +1,18 @@
-def load_data(path : str):
+from jax import jit
+import jax.numpy as jnp
+
+
+def load_data(path: str) -> str:
     with open(path) as file:
         return file.read()
 
-def save_data(path : str, data : str):
+
+def save_data(path: str, data: str) -> None:
     with open(path, "w") as f:
         f.write(data)
+
+
+@jit
+def softmax(x: jnp.ndarray, temp: float) -> jnp.ndarray:
+    x = jnp.exp(x / temp)
+    return x / jnp.mean(x)
